699d0ce6c0584d02bdd77706cdcf2955db4ba131
Sky Resendez <sky.resendez@gmail.com>
2018-08-29 11:41:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,59 @@
 # Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+def position_taken?(board, pos)
+  !(board[pos].nil? || board[index] == " ")
+end
+
+def won?(board)
+  top_row_win = [0,1,2]
+  middle_row_win = [3,4,5]
+
+  if board.include?(WIN_COMBINATIONS)
+    if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+      return top_row_win
+    end
+    if board[middle_row_win[3]] == "X" && board[middle_row_win[4]] == "X" && board[middle_row_win[5]] == "X"
+      return middle_row_win
+    end
+  else
+    return false
+  end
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # top row
+  [3,4,5], # middle rows
+  [6,7,8], # bottom row
+  [0,3,6], # left column
+  [1,4,7], # middle column
+  [2,5,8], # right column
+  [0,4,8], # diagonal one
+  [2,4,6] # diagonal two
+]
+
+
+=begin
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+top_row_win = [0,1,2]
+middle_row_win = [3,4,5]
+
+if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+  "X won in the top row"
+end
+
+if board[middle_row_win[3]] == "X" && board[middle_row_win[4]] == "X" && board[middle_row_win[5]] == "X"
+    "X won in the middle row"
+end
+  if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+    "X won in the top row"
+  end
+  if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+    "X won in the top row"
+  end
+  if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+    "X won in the top row"
+  end
+  if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+    "X won in the top row"
+  end
+=end
